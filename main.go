01e673ba0c4bd2859794d2656c480c16f6e61a74
Thottempudi, Harsha <harsha.02k@gmail.com>
2024-01-09 12:55:02 -0500

    respond to redis-cli ping

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func checkErr(err error){
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":6379")
+	checkErr(err)
+	fmt.Println("Listening on port 6379.")
+
+	conn, err := ln.Accept()
+	checkErr(err)
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err.Error())
+				panic(err)
+			}
+		}
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
